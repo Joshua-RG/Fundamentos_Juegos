095c4203c9bae150f2de3a0331f6f78c58f1fdfa
Joshua-RG <arguerrero06gmail.com>
2025-04-23 21:50:59 -0500

    Proyecto integrado con SDL, GLEW y generando VBO

---
+++ Juegos_1/MainGame.cpp
@@ -0,0 +1,70 @@
+#include "MainGame.h"
+
+void MainGame::init()
+{
+	SDL_Init(SDL_INIT_HAPTIC | SDL_INIT_CAMERA | SDL_INIT_EVENTS);
+	window = SDL_CreateWindow("TrALAlero TraLAla", width, height, SDL_WINDOW_OPENGL);
+
+	SDL_GLContext glContext = SDL_GL_CreateContext(window);
+	GLenum error = glewInit();
+	if (error != GLEW_OK) {}
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); //TRABAJA CON 2 PANTALLA PARA QUE NO SE VEA EL RENDERIZADO CONTINUO
+	glClearColor(0.5f, 0.0f, 1.0f, 1.0f);
+
+}
+
+void MainGame::processInput()
+{
+	SDL_Event event;
+	while (SDL_PollEvent(&event)) {
+		switch (event.type)
+		{
+		case SDL_EVENT_QUIT:
+			gameState = GameState::EXIT;
+			break;
+		case SDL_EVENT_MOUSE_MOTION:
+			cout << "posicion x " << event.motion.x << " posicion y " << event.motion.y << endl;
+			break;
+		}
+	}
+}
+
+MainGame::MainGame()
+{
+}
+
+MainGame::~MainGame()
+{
+}
+
+void MainGame::run()
+{
+	width = 800;
+	height = 800;
+	gameState = GameState::PLAY;
+	init();
+	sprite.init(-1, -1, 1, 1);
+	update();
+}
+
+void MainGame::update()
+{
+	while (gameState != GameState::EXIT) {
+		processInput();
+		draw();
+	}
+}
+
+// Que hace el or binario ( | )
+// GL_COLOR_BUFFER_BIT = 0x00004000
+// GL_DEPTH_BUFFER_BIT = 0x00000100
+// GL_DEPTH_BUFFER_BIT = 0x00004100
+
+void MainGame::draw()
+{
+	glClearDepth(1.0); //RESETEA LA POSICION DE LA CAMARA
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //BORRA EL FONDO Y VUELVE A FORZAR EL RESETEO DE LA CAMARA
+	sprite.draw();
+	SDL_GL_SwapWindow(window); //INTERCAMBIA LAS PANTALLAS PARA QUE SE VEA FLUIDO
+}
---
+++ Juegos_1/Sprite.cpp
@@ -0,0 +1,69 @@
+#include "Sprite.h"
+#include "Vertex.h"
+#include <cstddef>
+
+Sprite::Sprite()
+{
+}
+
+Sprite::~Sprite()
+{
+}
+
+void Sprite::init(float x, float y, int width, int height)
+{
+	this->x = x;
+	this->y = y;
+	this->width = width;
+	this->height = height;
+	if (vboID == 0) {
+		glGenBuffers(1, &vboID);
+	}
+	
+	/*float vertexData[12];
+
+	vertextData[0] = x + width;
+	vertextData[1] = y + height;
+	vertextData[2] = x;
+	vertextData[3] = y + height;
+	vertextData[4] = x;
+	vertextData[5] = y;
+	vertextData[6] = x;
+	vertextData[7] = y;
+	vertextData[8] = x + width;
+	vertextData[9] = y;
+	vertextData[10] = x + width;
+	vertextData[11] = y + height;*/
+
+	Vertex vertexData[6];
+
+	vertexData[0].setPosicion(x + width, y + height);
+	vertexData[1].setPosicion(x, y + height);
+	vertexData[2].setPosicion(x, y);
+	vertexData[3].setPosicion(x, y);
+	vertexData[4].setPosicion(x + width, y);
+	vertexData[5].setPosicion(x + width, y + height);
+
+	for (int i = 0; i < 6; i++)
+	{
+		vertexData[i].setColor(255, 0, 0, 255);
+	}
+
+	vertexData[1].setColor(0, 0, 255, 255);
+	vertexData[4].setColor(0, 255, 0, 255);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vboID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void Sprite::draw()
+{
+	glBindBuffer(GL_ARRAY_BUFFER, vboID); 
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex,position));
+	glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vertex), (void*)offsetof(Vertex, color))
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDisableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
